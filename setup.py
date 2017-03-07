f94efb7936ab88e4403365168a3c874c511cc28d
root <root@ubuntu.unassigned-domain>
2017-03-07 10:49:01 +0900

    Preparation

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 from setuptools import setup, find_packages
-from PyMODE-Device import __author__, __version__, __license__
+from mode-py import __author__, __version__, __license__
  
 setup(
-        name             = 'PyMODE-Device',
+        name             = 'mode-py',
         version          = __version__,
         description      = '',
         license          = __license__,
         author           = __author__,
         author_email     = '',
-        url              = 'https://github.com/pachicourse/PyMODE-Device.git',
+        url              = 'https://github.com/pachicourse/mode-py.git',
         keywords         = 'MODE Device pip github python',
         packages         = find_packages(),
         install_requires = [],
