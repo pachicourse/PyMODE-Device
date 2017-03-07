d429406989a3cd3a878de1e8c193edf9bef06fc9
root <root@ubuntu.unassigned-domain>
2017-03-07 10:23:25 +0900

    Preparation

---
+++ PyMODE-Device/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# coding: utf-8
+ 
+#import 
+
+__author__  = 'pachicourse'
+__version__ = '0.0.1'
+__license__ = 'MIT'
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+from setuptools import setup, find_packages
+from PyMODE-Device import __author__, __version__, __license__
+ 
+setup(
+        name             = 'PyMODE-Device',
+        version          = __version__,
+        description      = '',
+        license          = __license__,
+        author           = __author__,
+        author_email     = '',
+        url              = 'https://github.com/pachicourse/PyMODE-Device.git',
+        keywords         = 'MODE Device pip github python',
+        packages         = find_packages(),
+        install_requires = [],
+)
