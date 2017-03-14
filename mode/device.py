4d2a0c638b365e71971f48368fb1fba8b152c60b
root <root@ubuntu.unassigned-domain>
2017-03-14 17:41:18 +0900

    add try_reconnect() function

--- mode/device.py
+++ mode/device.py
@@ -4,19 +4,20 @@
 import requests
 import json
 import websocket
-
+import threading
 
 class Device:
 
 	def __init__(self):
-		self.host = 'api.tinkermode.com'
-		self.port = 443
-		self.devicId = ''
-		self.token = ''
-		self.on_open = default_on_open
-		self.on_message = default_on_message
-		self.on_error = default_on_error
-		self.on_close = default_on_close
+		self.host              = 'api.tinkermode.com'
+		self.port              = 443
+		self.devicId           = ''
+		self.token             = ''
+		self.on_open           = default_on_open
+		self.on_message        = default_on_message
+		self.on_error          = default_on_error
+		self.on_close          = default_on_close
+		self.websocket_debug   = False
 
 	def set_api_host(self, host):
 		self.host = host
@@ -25,6 +26,9 @@ class Device:
 		self.deviceId = deviceId
 		self.token = token
 
+	def websocket_trace(self, b):
+		self.websocket_debug = b
+
 	def trigger_event(self, eventType, eventData):
 		r = requests.put('https://' + self.host + '/devices/' + str(self.deviceId) + '/event', \
 				 json.dumps({"eventType":eventType, "eventData":eventData}), \
@@ -32,7 +36,7 @@ class Device:
 		print(r)	
 
 	def listen_commands(self):
-		websocket.enableTrace(True)
+		websocket.enableTrace(self.websocket_debug)
 		ws = websocket.WebSocketApp('wss://' + self.host + '/devices/' + str(self.deviceId) + '/command',
 					    header=["Authorization: ModeCloud %s" % self.token], on_open=self.on_open, \
 					    on_message=self.on_message, on_error=self.on_error, on_close=self.on_close)
@@ -45,8 +49,12 @@ class Device:
 			ws.close()
 
 # websocket callback
-# when open websocket
+wait = 0.5 #sec
+
+# when open websocke
 def default_on_open(ws):
+	global wait
+	wait = 0.5
 	print('connected')
 
 # when get message
@@ -56,7 +64,26 @@ def default_on_message(ws, message):
 # when error
 def default_on_error(ws, error):
 	print("error")
-	
+	global wait
+	if wait < 60:
+		wait *= 2 # first error => wait for 1sec.
+	__try_reconnect(ws)
+
 # when close websocket
 def default_on_close(ws):
 	print('disconnected')
+
+def __try_reconnect(ws):
+	print('wait for', wait, 'sec to reconnect')
+	t = threading.Timer(wait, __reconnect, args=[ws])
+	t.start();
+
+def __reconnect(ws):
+	print('reconnecting ..')
+	
+	#Start websocket
+	try:
+		ws.run_forever()
+	# Ctrl+C to close websocket
+	except KeyboardInterrupt:
+		ws.close()
--- test.py
+++ test.py
@@ -11,7 +11,7 @@ API_KEY = 'v1.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 mode_device.set_device_keys(DEVICE_ID, API_KEY)
 mode_device.set_api_host('iot-device.jp-east-1.api.cloud.nifty.com')
 
-mode_device.trigger_event('mode-py_test', {'value':'hoge'})
+#mode_device.trigger_event('mode-py_test', {'value':'hoge'})
 
 mode_device.on_message = test_func
 mode_device.listen_commands()
