81c1f82449e2b263f0759f6caf9a4740f53e63ce
root <root@ubuntu.unassigned-domain>
2017-03-16 11:58:26 +0900

    'wait' is no longer a global variable.'

--- mode/device.py
+++ mode/device.py
@@ -7,17 +7,14 @@ import websocket
 import threading
 
 class Device:
-
+	
 	def __init__(self):
 		self.host              = 'api.tinkermode.com'
 		self.port              = 443
 		self.devicId           = ''
 		self.token             = ''
-		self.on_open           = default_on_open
-		self.on_message        = default_on_message
-		self.on_error          = default_on_error
-		self.on_close          = default_on_close
 		self.websocket_debug   = False
+		self.wait              = 0.5
 
 	def set_api_host(self, host):
 		self.host = host
@@ -34,7 +31,7 @@ class Device:
 				 json.dumps({"eventType":eventType, "eventData":eventData}), \
 			         headers={'Content-Type':'application/json','Authorization':'ModeCloud ' + self.token})
 		print(r)	
-
+	
 	def listen_commands(self):
 		websocket.enableTrace(self.websocket_debug)
 		ws = websocket.WebSocketApp('wss://' + self.host + '/devices/' + str(self.deviceId) + '/command',
@@ -47,43 +44,40 @@ class Device:
 		# Ctrl+C to close websocket
 		except KeyboardInterrupt:
 			ws.close()
-
-# websocket callback
-wait = 0.5 #sec
-
-# when open websocke
-def default_on_open(ws):
-	global wait
-	wait = 0.5
-	print('connected')
-
-# when get message
-def default_on_message(ws, message):
-	print(message)
-
-# when error
-def default_on_error(ws, error):
-	print("error")
-	global wait
-	if wait < 60:
-		wait *= 2 # first error => wait for 1sec.
-	__try_reconnect(ws)
-
-# when close websocket
-def default_on_close(ws):
-	print('disconnected')
-
-def __try_reconnect(ws):
-	print('wait for', wait, 'sec to reconnect')
-	t = threading.Timer(wait, __reconnect, args=[ws])
-	t.start();
-
-def __reconnect(ws):
-	print('reconnecting ..')
 	
-	#Start websocket
-	try:
-		ws.run_forever()
-	# Ctrl+C to close websocket
-	except KeyboardInterrupt:
-		ws.close()
+	# websocket callback methods
+	# when open websocke
+	def on_open(self, ws):
+		self.wait = 0.5
+		print('connected')
+
+	# when get message
+	def on_message(ws, message):
+		print(message)
+
+	# when error
+	def on_error(self, ws, error):
+		print("error")
+		if self.wait < 60:
+			self.wait *= 2 # first error => wait for 1sec.
+		print(self.wait)
+		self._try_reconnect(ws)
+	
+	# when close websocket
+	def on_close(self, ws):
+		print('disconnected')
+
+	def _try_reconnect(self, ws):
+		print('wait for', self.wait, 'sec to reconnect')
+		t = threading.Timer(self.wait, self._reconnect, args=[ws])
+		t.start();
+
+	def _reconnect(self, ws):
+		print('reconnecting ..')
+		
+		#start websocket
+		try:
+			ws.run_forever()
+		# Ctrl+C to close websocket
+		except KeyboardInterrupt:
+			ws.close()
